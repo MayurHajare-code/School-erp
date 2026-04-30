68accf40dc2decd6a965a37635d6b8f0e151dd8f
MayurHajare-code <147743627+MayurHajare-code@users.noreply.github.com>
2026-04-30 09:53:54 +0530

    update layout.css file name

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import "../styles/layout.css";
+import "../styles/layout.css"; //changing css file name
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import { useState } from "react";
